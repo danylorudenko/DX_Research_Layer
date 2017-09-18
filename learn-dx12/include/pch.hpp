7578c80aa6c1f29204b3a06a2e5f937fe78f80b3
Danylo Rudenko <rudenko.dany@gmail.com>
2017-09-18 16:04:38 +0300

    Hello triangle development[1].

--- learn-dx12/include/pch.hpp
+++ learn-dx12/include/pch.hpp
@@ -11,6 +11,13 @@
 #include <d3d12.h>
 #include <d3dx12.h>
 
+#include <DirectXMath.h>
+
+#include <d3dcompiler.h>
+
+#include <Utility\\Vertex.hpp>
+#include <Utility\\SceneConstantBuffer.hpp>
+
 #ifndef ThrowIfFailed
 #define ThrowIfFailed(result) { HRESULT hr__ = (result); if(FAILED(hr__)) { std::string msg = "HRESULT failed in file: "; msg += __FILE__; msg += ", line: "; msg += std::to_string(__LINE__); throw std::exception(msg.c_str()); } }
 #endif
