534965ee1cdd2ecf0361bdd13aaf77abbef57575
Danylo Rudenko <rudenko.dany@gmail.com>
2017-10-11 11:13:10 +0300

    Camera math implementation.

--- learn-dx12/include/Math/Camera.hpp
+++ learn-dx12/include/Math/Camera.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <pch.hpp>
+#include <Math\Transform.hpp>
 
 class Camera
 {
@@ -13,15 +14,27 @@ public:
     Camera& operator=(Camera&&);
 
     float Fow() const { return fow_; }
-    float AspectRation() const { return aspectRatio_; }
+    float AspectRatio() const { return aspectRatio_; }
     float NearPlane() const { return nearPlane_; }
     float FarPlane() const { return farPlane_; }
 
-    DirectX::XMFLOAT4X4A ViewMatrix() const;
-    DirectX::XMMATRIX XM_CALLCONV ViewMatrixSIMD() const;
+    void Fow(float fow) { fow_ = fow; perspectiveMatrixDirty_ = true; }
+    void NearPlane(float nearPlane) { nearPlane_ = nearPlane; perspectiveMatrixDirty_ = true; }
+    void FarPlane(float farPlane) { farPlane_ = farPlane; perspectiveMatrixDirty_ = true; }
+    void AspectRatio(float aspect) { aspectRatio_ = aspect; perspectiveMatrixDirty_ = true; }
+
+    DirectX::XMFLOAT4X4A ViewMatrix(DirectX::XMFLOAT3A& pos, DirectX::XMFLOAT3A& direction, DirectX::XMFLOAT3A& up) const;
+    DirectX::XMFLOAT4X4A XM_CALLCONV ViewMatrix(DirectX::FXMVECTOR pos, DirectX::FXMVECTOR direction, DirectX::FXMVECTOR up) const;
+
+    DirectX::XMMATRIX XM_CALLCONV ViewMatrixSIMD(DirectX::XMFLOAT3A& pos, DirectX::XMFLOAT3A& direction, DirectX::XMFLOAT3A& up) const;
+    DirectX::XMMATRIX XM_CALLCONV ViewMatrixSIMD(DirectX::FXMVECTOR pos, DirectX::FXMVECTOR direction, DirectX::FXMVECTOR up) const;
+
+    DirectX::XMFLOAT4X4A PerspectiveMatrix();
+    DirectX::XMMATRIX XM_CALLCONV PerspectiveMatrixSIMD() const;
 
 private:
-    DirectX::XMFLOAT4X4A viewMatrix_;
+    DirectX::XMFLOAT4X4A perspectiveMatrix_;
+    bool perspectiveMatrixDirty_ = true;
 
     float fow_;
     float aspectRatio_;
--- learn-dx12/src/Camera.cpp
+++ learn-dx12/src/Camera.cpp
@@ -1,7 +1,6 @@
 #include <Math\Camera.hpp>
 
 Camera::Camera(float fow, float aspectRatio, float nearPlane, float farPlane) :
-    viewMatrix_{},
     fow_(fow),
     aspectRatio_(aspectRatio_),
     nearPlane_(nearPlane),
@@ -17,3 +16,43 @@ Camera::Camera(Camera&&) = default;
 Camera& Camera::operator=(const Camera&) = default;
 
 Camera& Camera::operator=(Camera&&) = default;
+
+DirectX::XMFLOAT4X4A Camera::ViewMatrix(DirectX::XMFLOAT3A& pos, DirectX::XMFLOAT3A& direction, DirectX::XMFLOAT3A& up) const
+{
+    DirectX::XMFLOAT4X4A viewMatrix;
+    DirectX::XMStoreFloat4x4A(&viewMatrix, ViewMatrixSIMD(DirectX::XMLoadFloat3A(&pos), DirectX::XMLoadFloat3A(&direction), DirectX::XMLoadFloat3A(&up)));
+    return viewMatrix;
+}
+
+DirectX::XMFLOAT4X4A Camera::ViewMatrix(DirectX::FXMVECTOR pos, DirectX::FXMVECTOR direction, DirectX::FXMVECTOR up) const
+{
+    DirectX::XMFLOAT4X4A viewMatrix;
+    DirectX::XMStoreFloat4x4A(&viewMatrix, ViewMatrixSIMD(pos, direction, up));
+
+    return viewMatrix;
+}
+
+DirectX::XMMATRIX Camera::ViewMatrixSIMD(DirectX::FXMVECTOR pos, DirectX::FXMVECTOR direction, DirectX::FXMVECTOR up) const
+{
+    return DirectX::XMMatrixLookToLH(pos, direction, up);
+}
+
+DirectX::XMMATRIX Camera::ViewMatrixSIMD(DirectX::XMFLOAT3A& pos, DirectX::XMFLOAT3A& direction, DirectX::XMFLOAT3A& up) const
+{
+    return ViewMatrixSIMD(DirectX::XMLoadFloat3A(&pos), DirectX::XMLoadFloat3A(&direction), DirectX::XMLoadFloat3A(&up));
+}
+
+DirectX::XMFLOAT4X4A Camera::PerspectiveMatrix()
+{
+    if (perspectiveMatrixDirty_) {
+        DirectX::XMStoreFloat4x4A(&perspectiveMatrix_, PerspectiveMatrixSIMD());
+        perspectiveMatrixDirty_ = false;
+    }
+
+    return perspectiveMatrix_;
+}
+
+DirectX::XMMATRIX Camera::PerspectiveMatrixSIMD() const
+{
+    return DirectX::XMMatrixPerspectiveFovLH(DirectX::XM_PI / 180 * fow_, aspectRatio_, nearPlane_, farPlane_);
+}
