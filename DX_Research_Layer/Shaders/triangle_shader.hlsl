6c325dd352d6dae7fe0bb9c3b4671434c7dfb693
Danylo Rudenko <rudenko.dany@gmail.com>
2018-03-11 19:15:35 +0200

    Buggy Cook-Torrance BRDF implementation.
    Signed-off-by: Danylo Rudenko <rudenko.dany@gmail.com>

--- DX_Research_Layer/Shaders/triangle_shader.hlsl
+++ DX_Research_Layer/Shaders/triangle_shader.hlsl
@@ -37,6 +37,24 @@ PSInput VS(float3 position : POSITION, float3 normal : NORMAL)
     return result;
 }
 
+float TrowbridgeReitzGGXNDF(float ndoth, float roughness)
+{
+	float nominator = roughness * roughness;
+	float denominator = ndoth * ndoth * (nominator - 1.0f) + 1.0f;
+	denominator = PI * denominator * denominator;
+
+	return nominator / max(denominator, 0.001f);
+}
+
+float ShlickGGX(float ndotv, float roughness)
+{
+	float k = pow(roughness + 1, 2) / 8.0f;
+
+	float denominator = (ndotv * (1 - k)) + k;
+
+	return ndotv / max(denominator, 0.001f);
+}
+
 float3 ShlickFresnel(float3 Rf0, float angleCos)
 {
 	float coef = 1 - angleCos;
@@ -45,35 +63,60 @@ float3 ShlickFresnel(float3 Rf0, float angleCos)
 	return reflectance;
 }
 
+float3 CookTorranceBRDF(float ndf, float schlickGGX, float3 fresnel, float ndotv, float ndotl)
+{
+	float3 nominator = ndf * schlickGGX * fresnel;
+	float denominator = 4 * (ndotv) * (ndotl);
+
+	return nominator / max(denominator, 0.001f);
+}
+
+float3 CombineDiffuseSpecular(float3 BRDF, float3 diffuse, float ndotl, float metalness)
+{
+	float3 kS = BRDF;
+	float3 kD = float3(1.0f, 1.0f, 1.0f) - kS;
+	kD *= 1.0f - metalness;
+
+	float3 result = (diffuse * kD) + (BRDF * ndotl);
+	return result;
+}
+
 float4 PS(PSInput input) : SV_TARGET
 {
     //float3 ambient = pow(float3(0.2f, 0.3f, 0.5f), 2.2f) * Lambertian * 0.5f * (1.0f + dot(float3(0.0f, 1.0f, 0.0f), normalize(input.NormW)));
 
-	const float  m = 1024.0f;
-	const float3 R0 = float3(0.02f, 0.02f, 0.02f);
-
-    const float3 l = normalize(float3(1.0f, 1.0f, 0.0f));
     const float3 baseDiffColor = float3(0.0f, 215.0f, 255.0f ) * CLAMP_256;
     const float3 baseSpecColor = float3(0.5f, 0.5f, 0.5f);
 
     const float3 baseLinDiffColor = pow(baseDiffColor, TO_LINEAR);
     const float3 baseLinSpecColor = pow(baseSpecColor, TO_LINEAR);
 
+
+	const float  roughness = 0.01f;
+	const float  metalness = 0.9f;
+		  float3 R0 = float3(0.04f, 0.04f, 0.04f);
+				 R0 = lerp(R0, baseLinSpecColor, metalness);
+
+
+    const float3 l = normalize(float3(1.0f, 1.0f, 0.0f));
 	const float3 n = normalize(input.NormW);
 	const float3 v = normalize(cameraPosition - input.PosW);
 	const float3 h = normalize((l + v) / 2);
-	const float3 nF = (m + 8) / (8 * PI);
-	const float  ndotl = dot(n, l);
-	const float  ndoth = dot(n, h);
-	const float  ndotv = dot(n, v);
+	const float  ndotl = max(dot(n, l), 0.0f);
+	const float  ndoth = max(dot(n, h), 0.0f);
+	const float  ndotv = max(dot(n, v), 0.0f);
 
-	float3 fresnelReflectance =  ShlickFresnel(R0, ndotv);
+	const float3 fresnel	= ShlickFresnel(R0, ndotv);
+	const float  ndf		= TrowbridgeReitzGGXNDF(ndoth, roughness);
+	const float  g			= ShlickGGX(ndotv, roughness);
+	const float3 specLin	= CookTorranceBRDF(ndf, g, fresnel, ndotv, ndotl);
 
-	const float3 specLin = pow(ndoth, m) * nF * fresnelReflectance * baseLinSpecColor;
 
+	const float3 resultLinDiffColor = ndotl * baseLinDiffColor * LAMBERTIAN;
+	//const float3 resultLinDiffColor = float3(0.0f, 0.0f, 0.0f);
 
-	const float3 resultLinColor = ndotl * baseLinDiffColor * LAMBERTIAN;
-	const float3 resultColor = pow(resultLinColor, TO_GAMMA) + pow(specLin, TO_GAMMA);
+	const float3 resultLin = CombineDiffuseSpecular(resultLinDiffColor, specLin, ndotl, metalness);
+	const float3 resultColor = pow(resultLin, TO_GAMMA);
 
     return float4(resultColor, 1.0f);
 
