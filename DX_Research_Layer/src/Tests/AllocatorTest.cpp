0d238cd7ffcac5a86860a94636bae3d05f4b4557
Danylo Rudenko <rudenko.dany@gmail.com>
2018-04-21 20:19:42 +0300

    Simple linear memory allocation test.
    Signed-off-by: Danylo Rudenko <rudenko.dany@gmail.com>

--- DX_Research_Layer/src/Tests/AllocatorTest.cpp
+++ DX_Research_Layer/src/Tests/AllocatorTest.cpp
@@ -10,85 +10,26 @@ struct CharBuffer32
     char content[32];
 };
 
-struct CharBuffer256
-{
-    char content[256];
-};
-
-struct CharBuffer1024
-{
-    char content[1024];
-};
-
 int main()
 {
     using namespace DXRL;
-
-    std::random_device device{};
-    std::default_random_engine engine{ device() };
-
-    std::uniform_int_distribution<> uniform_dist{ 100, 10000 };
     
-    std::string const testString{ "THIS IS A TEST STRING!" };
-    Size constexpr allocationsCount = 10000;
+    char* const testString{ "012345678901234567890123456789" };
 
     ///////////////////////////////////////////////////
 
-    Memory::VoidPtr* linearAllocations = new Memory::VoidPtr[allocationsCount];
-    Memory::VoidPtr* newAllocations = new Memory::VoidPtr[allocationsCount];
+    Size constexpr allocationsCount = 10;
+    //CharBuffer32* bufferPtrArray[allocationsCount];
 
-
-    Memory::Bytes linearChunkSize{ allocationsCount * 10000 };
+    Memory::Bytes linearChunkSize{ sizeof(CharBuffer32) * allocationsCount };
     Memory::VoidPtr linearMemoryChunk = malloc(linearChunkSize);
     Memory::LinearAllocator allocator{ linearMemoryChunk, linearChunkSize, true };
 
-    //////////////////////////////////////
-    // LINEAR TEST START
-    auto linearStart = std::chrono::high_resolution_clock::now();;
-
     for (Size i = 0; i < allocationsCount; ++i) {
-        char* ptr = reinterpret_cast<char*>(allocator.Alloc(uniform_dist(engine)));
-        strcpy_s(ptr, 80, testString.c_str());
+        CharBuffer32* buffer = allocator.Alloc<CharBuffer32>();
+        strcpy_s(buffer->content, sizeof(CharBuffer32), testString);
     }
-
-    allocator.Reset();
-
-    auto linearEnd = std::chrono::high_resolution_clock::now();
     
-    // LINEAR TEST END
-    //////////////////////////////////////
-    auto linearDuration = linearEnd - linearStart;
-    std::cout << allocationsCount << " linear allocations: " << linearDuration.count() << " nanoseconds" << std::endl << std::endl;
-
-
-
-
-    //////////////////////////////////////
-    // NEW TEST START
-    auto newStart = std::chrono::high_resolution_clock::now();;
-
-    for (Size i = 0; i < allocationsCount; ++i) {
-        char* ptr = reinterpret_cast<char*>(malloc(uniform_dist(engine)));
-        *(newAllocations + i) = ptr;
-        strcpy_s(ptr, 80, testString.c_str());
-    }
-
-    for (Size i = 0; i < allocationsCount; ++i) {
-        free(*(newAllocations + i));
-    }
-
-    auto newEnd = std::chrono::high_resolution_clock::now();;
-    // NEW TEST END
-    //////////////////////////////////////
-
-    auto newDuration = newEnd - newStart;
-    std::cout << allocationsCount << " ___new allocations: " << newDuration.count() << " nanoseconds" << std::endl << std::endl;
-
-    ////////////////////////////////////////////////////
-
-    delete[] linearAllocations;
-    delete[] newAllocations;
-
     system("pause");
     return 0;
 }
